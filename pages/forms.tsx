edc12d5727f1b1823c726d1a9c25939eb2f77e84
kky8822 <99176588+kky8822@users.noreply.github.com>
2022-06-24 22:28:48 +0900

    220624_react-hook-form

---
+++ pages/forms.tsx
@@ -0,0 +1,54 @@
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface IForm {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export default function Forms() {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({ mode: "onChange" });
+  const onValid = (data: IForm) => {
+    console.log("im valid");
+  };
+  const onInvalid = (errors: FieldErrors) => {};
+  console.log(errors);
+
+  return (
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
+      <input
+        {...register("username", {
+          required: "Username is required",
+          minLength: { value: 5, message: "username should be longer than 5" },
+        })}
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        {...register("email", {
+          required: "Email is required",
+          validate: {
+            notGmail: (value) =>
+              !value.includes("@gmail.com") || "gmail is not allowed",
+          },
+        })}
+        type="email"
+        placeholder="Email"
+      />
+      {errors.email?.message}
+      <input
+        {...register("password", {
+          required: "Password is required",
+        })}
+        type="password"
+        placeholder="Password"
+      />
+      <input type="submit" value="create" />
+    </form>
+  );
+}
