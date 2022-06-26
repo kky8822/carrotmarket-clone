200abfc6089e8fa5aa6b99d270c7188408599215
kky8822 <99176588+kky8822@users.noreply.github.com>
2022-06-26 19:13:04 +0900

    220626_AUTHENTICATION_FIN

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import Button from "@components/button";
 import Input from "@components/input";
 import useMutation from "@libs/client/useMutation";
 import { cls } from "@libs/client/utils";
+import { useRouter } from "next/router";
 
 interface EnterForm {
   email?: string;
@@ -45,8 +46,13 @@ export default function Enter() {
     if (tokenLoading) return;
     confirmToken(validForm);
   };
+  const router = useRouter();
+  useEffect(() => {
+    if (tokenData?.ok) {
+      router.push("/");
+    }
+  }, [tokenData, router]);
 
-  console.log(loading, data, error);
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
@@ -64,7 +70,7 @@ export default function Enter() {
               required={true}
             />
 
-            <Button text={loading ? "Loading" : "Confirm Token"} />
+            <Button text={tokenLoading ? "Loading" : "Confirm Token"} />
           </form>
         ) : (
           <>
